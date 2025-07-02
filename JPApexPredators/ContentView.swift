d33a358f88536e8945a401e15b9bfe3ff10e6e0a
Kaiser Hussain <ksr.hssn@gmail.com>
2025-07-02 15:11:55 -0400

    End of Lesson 8

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -10,36 +10,87 @@ import SwiftUI
 struct ContentView: View {
     let predators = Predators()
     
+    @State var searchText: String = ""
+    @State var alphabetical = false
+    @State var currentSelection = APType.all
+    
+    var filteredDinos: [ApexPredator] {
+        predators.filter(by: currentSelection)
+        
+        predators.sort(by: alphabetical)
+        
+        return predators.search(for: searchText)
+    }
+    
     var body: some View {
-        List(predators.apexPredators) { predator in
-            HStack {
-                // Dinosaur image
-                Image(predator.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                    .shadow(color: .white, radius: 1)
-                
-                VStack(alignment: .leading) {
-                    // Name
-                    Text(predator.name)
-                        .fontWeight(.bold)
+        NavigationStack {
+            List(filteredDinos) { predator in
+                NavigationLink {
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                } label: {
                     
-                    // Type
-                    Text(predator.type.rawValue.capitalized)
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .padding(.horizontal, 13)
-                        .padding(.vertical, 5)
-                        .background(predator.type .background)
-                        .clipShape(.capsule)
+                    HStack {
+                        // Dinosaur image
+                        Image(predator.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .shadow(color: .white, radius: 1)
                         
+                        VStack(alignment: .leading) {
+                            // Name
+                            Text(predator.name)
+                                .fontWeight(.bold)
+                            
+                            // Type
+                            Text(predator.type.rawValue.capitalized)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 13)
+                                .padding(.vertical, 5)
+                                .background(predator.type .background)
+                                .clipShape(.capsule)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Apex Predators")
+            .searchable(text: $searchText)  // Adds search bar
+            .autocorrectionDisabled()
+            .animation(.default, value: searchText)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        withAnimation {
+                            alphabetical.toggle()
+                        }
+                    } label: {
+                        Image(systemName: alphabetical ? "film" : "textformat")
+                            .symbolEffect(.bounce, value: alphabetical)
+                    }
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Menu {
+                        Picker("Filter", selection:
+                                $currentSelection.animation()) {
+                            
+                            ForEach(APType.allCases) {
+                                type in
+                                Label(type.rawValue.capitalized, systemImage: type.icon)
+                            }
+                        }
+                    } label: {
+                        Image(systemName:
+                                "slider.horizontal.3")
+                    }
                 }
             }
         }
         .preferredColorScheme(.dark)
     }
-        
 }
 
 #Preview {
